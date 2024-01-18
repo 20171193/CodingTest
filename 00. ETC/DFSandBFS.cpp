2f3212a3d874e70967f01f29956a7e4aec22ed12
Shim Jae Chun <shimnaldo98@gmail.com>
2024-01-18 11:30:05 +0900

    백준 1260번 DFS와 BFS  : #그래프탐색 #DFS #BFS

---
+++ 00. ETC/DFSandBFS.cpp
@@ -0,0 +1,67 @@
+// 백준 1260번 DFS와 BFS
+
+#include <iostream>
+#include <queue>
+#include <vector>
+using namespace std;
+
+vector<int> Node[1001];
+bool visited[1001];
+int n, m, v;
+
+void DFS(int curNode)
+{
+	if (visited[curNode]) return;
+
+	visited[curNode] = true;
+	cout << curNode << " ";
+
+	for (int i = 0; i < Node[curNode].size(); i++)
+	{
+		int nextNode = Node[curNode][i];
+		if (visited[nextNode]) continue;
+		DFS(nextNode);	// 재귀
+	}
+}
+void BFS(int curNode)
+{
+	queue<int> q;
+	q.push(curNode);
+	visited[curNode] = true;
+	cout << curNode << " ";
+	while (q.size())
+	{
+		int cur = q.front(); q.pop();
+		for (int i = 0; i < Node[cur].size(); i++)
+		{
+			int nextNode = Node[cur][i];
+			if (visited[nextNode]) continue;
+
+			cout << nextNode << " ";
+			visited[nextNode] = true;
+			q.push(nextNode);
+		}
+	}
+
+
+}
+
+int main()
+{
+	cin.tie(NULL);
+	iostream::sync_with_stdio(false);
+
+	cin >> n >> m >> v;
+	for (int i = 0; i < m; i++)
+	{
+		int a = 0, b = 0;
+		cin >> a >> b;
+		Node[a].push_back(b);
+		Node[b].push_back(a);
+	}
+	DFS(v);
+	memset(visited, false, sizeof(visited));	// visited 배열 초기화
+	cout << '\n';
+	BFS(v);
+	return 0;
+}
